a35b8c6b8ac38df3653113ae3631cfe5b780e0e7
Eitan Bar-David <eitansbd@gmail.com>
2020-01-06 15:09:29 +0000

    Updates

---
+++ precache-manifest.2e23ed2a0950bf352f9f6586587bb373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5d2720fa802e4621184d0e83d967bf",
+    "url": "/fishermans-weather/index.html"
+  },
+  {
+    "revision": "4001e4db49395e7575ad",
+    "url": "/fishermans-weather/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "7296fdbca79e6fd68544",
+    "url": "/fishermans-weather/static/css/main.491aacc9.chunk.css"
+  },
+  {
+    "revision": "4001e4db49395e7575ad",
+    "url": "/fishermans-weather/static/js/2.8930f76a.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/fishermans-weather/static/js/2.8930f76a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7296fdbca79e6fd68544",
+    "url": "/fishermans-weather/static/js/main.3039c25d.chunk.js"
+  },
+  {
+    "revision": "7ef24f8953e0de81868e",
+    "url": "/fishermans-weather/static/js/runtime-main.431162ca.js"
+  }
+]);
